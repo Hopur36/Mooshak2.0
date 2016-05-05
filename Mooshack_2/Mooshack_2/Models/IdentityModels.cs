1e68b14b61f65d6b737f6b024db6dadcaa6e8327
evas14 <evas14@ru.is>
2016-05-05 11:28:13 +0000

    Vinna við forsíðu

--- Mooshack_2/Mooshack_2/Models/IdentityModels.cs
+++ Mooshack_2/Mooshack_2/Models/IdentityModels.cs
@@ -21,22 +21,15 @@ namespace Mooshack_2.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-<<<<<<< Updated upstream
 
-        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Course>        Courses         { get; set; }
         public DbSet<Assignment>    Assignments     { get; set; }
-        public DbSet<CourseStudent> CourseStudents { get; set; }
-        public DbSet<CourseTeacher> CourseTeachers { get; set; }
+        public DbSet<CourseStudent> CourseStudents  { get; set; }
+        public DbSet<CourseTeacher> CourseTeachers  { get; set; }
         public DbSet<Milestone>     Milestones      { get; set; }
         public DbSet<Submission>    Submissions     { get; set; }
-=======
-        //public DbSet<Assignment>    Assignments     { get; set; }
-        //public DbSet<Course>        Courses         { get; set; }
-        //public DbSet<CourseStudent> CourcesStudents { get; set; }
-        //public DbSet<CourseTeacher> CourcesTeachers { get; set; }
-        //public DbSet<Milestone>     Milestones      { get; set; }
-        //public DbSet<Submission>    Submissions     { get; set; }
->>>>>>> Stashed changes
+
         
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
