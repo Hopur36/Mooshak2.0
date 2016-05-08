aa108f15d6406d43ece74cc3159d56b28c79b8a6
bennivagn <benediktvb15@ru.is>
2016-05-08 11:26:22 +0000

    Búinn að bæta við courseteacherpage og virkni
    núna er hægt að sækja bara áfanga sem kennari kennir í gegnum service
    klasa. bjó líka til courseViewModel

--- Mooshack_2/Mooshack_2/Controllers/CourseController.cs
+++ Mooshack_2/Mooshack_2/Controllers/CourseController.cs
@@ -1,21 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using Mooshack_2.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Mooshack_2.Models;
 
 namespace Mooshack_2.Controllers
 {
     public class CourseController : Controller
     {
+        CourseService _courseService;
+        
+        public CourseController()
+        {
+            _courseService = new CourseService();
+        }
+
         // GET: Course
         public ActionResult Index()
         {
             return View();
         }
+
+        /// <summary>
+        /// This function gets a list of courseviewmodel returns a view
+        /// </summary>
+        /// <returns>View</returns>
+        [Authorize(Roles ="Teacher")]
         public ActionResult TeacherCoursePage()
         {
-            return View();
+            var _courses = _courseService.getAllCoursesByTeacherID(User.Identity.GetUserId());
+
+            return View(_courses);
         }
     }
 }
---
+++ Mooshack_2/Mooshack_2/Models/ViewModels/CourseViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mooshack_2.Models.ViewModels
+{
+    /// <summary>
+    /// a ViewModel for Course Entiti class
+    /// </summary>
+    public class CourseViewModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
