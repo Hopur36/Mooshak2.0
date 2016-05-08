41d540c8b7543ff7b6bcc7bea8e2e584cbec7470
bennivagn <benediktvb15@ru.is>
2016-05-08 20:45:58 +0000

    Núna getur kennari séð öll verkefni í völdum áfanga
    Núna getur kennari séð öll verkefni í völdum áfanga

--- Mooshack_2/Mooshack_2/Controllers/AssignmentController.cs
+++ Mooshack_2/Mooshack_2/Controllers/AssignmentController.cs
@@ -9,15 +9,33 @@ namespace Mooshack_2.Controllers
 {
     public class AssignmentController : Controller
     {
+        AssignmentService _assignmentService = new AssignmentService();
+
+        public AssignmentController()
+        {
+            _assignmentService = new AssignmentService();
+        }
+
         // GET: Assignment
         public ActionResult Index()
         {
-            var _assignmentService = new AssignmentService();
-            //var _assignmentModels = _assignmentService.getAllAssignmentViewModels();
-            var _assignmentModels = _assignmentService.getAssignmentByCourseID(4);
-            return View(_assignmentModels);
+
+            return View();
         }
-        
+
+        public ActionResult TeacherAssingmentPage(int? courseID)
+        {
+            if (courseID != null)
+            {
+                var _assignmentService = new AssignmentService();
+                var _assignmentModels = _assignmentService.getAssignmentByCourseID(courseID);
+                return View(_assignmentModels);
+
+            }
+
+            return View();
+        }
+
         public ActionResult TeacherNewAssignmentPage()
         {
             return View();
