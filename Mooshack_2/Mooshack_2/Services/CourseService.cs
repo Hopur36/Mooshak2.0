40e4be92d5c93481c06c38217372a6b08033f547
evas14 <evas14@ru.is>
2016-05-12 14:49:28 +0000

    breytingar á admin útliti

--- Mooshack_2/Mooshack_2/Services/CourseService.cs
+++ Mooshack_2/Mooshack_2/Services/CourseService.cs
@@ -441,5 +441,20 @@ namespace Mooshack_2.Services
             return true;
         }
 
+        public List<UserViewModel> getAllUsers()
+        {
+            var _allUsers = _dbContext.Users.ToList();
+
+            List<UserViewModel> _userViewModelList = new List<UserViewModel>();
+
+            foreach (var user in _allUsers)
+            {
+                _userViewModelList.Add(new UserViewModel { Id = user.Id, UserName = user.UserName, Email = user.Email });
+            }
+
+            _userViewModelList.Sort((x, y) => x.UserName.CompareTo(y.UserName));
+            return _userViewModelList;
+        }
+
     }
 }
